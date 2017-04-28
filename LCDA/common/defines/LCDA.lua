8c9651229f4821b0b9dd81fa6fb2d50372b6b0fa
Arko <gg.guidon@gmail.com>
2017-04-28 18:53:54 +0200

    defines

---
+++ LCDA/common/defines/LCDA.lua
@@ -0,0 +1,6 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+	NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+	NDefines.NMilitary.COASTAL_SUPPLY_BONUS = 0.99
+	NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 1000
