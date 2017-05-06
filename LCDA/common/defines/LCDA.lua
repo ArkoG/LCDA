00b5886c596c392e4205721954d5f1415329c482
Arko <gg.guidon@gmail.com>
2017-05-06 13:30:29 +0200

    regions creation and adjusting

--- LCDA/common/defines/LCDA.lua
+++ LCDA/common/defines/LCDA.lua
@@ -1,6 +1,6 @@
 -- Format for overwriting define values:
 --
 -- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
-	NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 3.0
+	NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.5
 	NDefines.NMilitary.COASTAL_SUPPLY_BONUS = 1.0
 	NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 1000
