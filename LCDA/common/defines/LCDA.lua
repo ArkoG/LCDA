4316ee16efdebd83e0380503f0625268315ec65c
Arko <gg.guidon@gmail.com>
2018-08-11 16:01:31 +0200

    change to fort holding premice

--- LCDA/common/defines/LCDA.lua
+++ LCDA/common/defines/LCDA.lua
@@ -4,3 +4,4 @@
 	NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.5
 	NDefines.NMilitary.COASTAL_SUPPLY_BONUS = 1.0
 	NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 1000
+	NDefines.NEconomy.FORT_CONSUMED_IN_SETTLEMENT_CONTRUCTION = 0		-- If set to 1 then fort holdings are consumed by the construction of another holding in the province while giving a discount to the construction cost in return.
