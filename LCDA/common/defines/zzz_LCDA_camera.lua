f08d7aabe2afc7e7ea4c992ff3ef841f6aa1ceb7
Arko <gg.guidon@gmail.com>
2018-08-25 01:48:42 +0200

    playing with camera

---
+++ LCDA/common/defines/zzz_LCDA_camera.lua
@@ -0,0 +1,52 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+	NDefines.NFrontend.CAMERA_LOOKAT_X = 1500.0 			-- Rotation point in main menu
+	NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
+	NDefines.NFrontend.CAMERA_LOOKAT_Z = 1500.0
+	NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1150.0  -- Rotation point in settings
+	NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Y = 0.0		-- Y is height
+	NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1200.0
+	NDefines.NFrontend.CAMERA_START_X = 2000.0				-- Initial position in main menu
+	NDefines.NFrontend.CAMERA_START_Y = 100.0				-- Y is height
+	NDefines.NFrontend.CAMERA_START_Z = 2000.0
+	NDefines.NFrontend.CAMERA_END_X = 2700.0				-- Move to position in main menu
+	NDefines.NFrontend.CAMERA_END_Y = 400.0
+	NDefines.NFrontend.CAMERA_END_Z = 2700.0
+	NDefines.NFrontend.TIME_FROZEN = 1.0  				-- Time before initial animation starts (some delay here so it should NOT be 0 then the animation starts before you can see it)
+	NDefines.NFrontend.GUI_MOVE_SPEED = 800				-- How fast sliding gui objects move ( pixels/s )
+	NDefines.NFrontend.MAX_ZOOM_LEVEL = 2500				-- How far out the player can zoom. Warning: At very high zoom levels the ground will no longer render
+	
+	NDefines.NFrontend.FADE_IN_DONE_TIME = 2.2
+	NDefines.NFrontend.GUI_START_MOVE_TIME = 1.4
+	NDefines.NFrontend.CAMERA_START_MOVE_TIME = 3.0
+	
+	NDefines.NFrontend.CAMERA_SPEED_IN_MENUS = 0.035
+	
+	NDefines.NFrontend.FRONTEND_POS_X = 1000.0
+	NDefines.NFrontend.FRONTEND_POS_Y = 200.0  -- atlitude
+	NDefines.NFrontend.FRONTEND_POS_Z = 1550.0  -- Y pos
+	NDefines.NFrontend.FRONTEND_LOOK_X = 1350.0
+	NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0  -- altitude
+	NDefines.NFrontend.FRONTEND_LOOK_Z = 1750.0  -- Y pos
+	
+	NDefines.NFrontend.SETTINGS_POS_X = 950.0
+	NDefines.NFrontend.SETTINGS_POS_Y = 400.0
+	NDefines.NFrontend.SETTINGS_POS_Z = 700.0
+	NDefines.NFrontend.SETTINGS_LOOK_X = 1350.0
+	NDefines.NFrontend.SETTINGS_LOOK_Y = 0.0		
+	NDefines.NFrontend.SETTINGS_LOOK_Z = 1300.0
+	
+	NDefines.NFrontend.MP_OPTIONS_POS_X = 360.0
+	NDefines.NFrontend.MP_OPTIONS_POS_Y = 200.0
+	NDefines.NFrontend.MP_OPTIONS_POS_Z = 848.0
+	NDefines.NFrontend.MP_OPTIONS_LOOK_X = 490.0
+	NDefines.NFrontend.MP_OPTIONS_LOOK_Y = 0.0	
+	NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1200.0
+	
+	NDefines.NFrontend.CREDITS_POS_X = 1000.0
+	NDefines.NFrontend.CREDITS_POS_Y = 200.0
+	NDefines.NFrontend.CREDITS_POS_Z = 1308.0
+	NDefines.NFrontend.CREDITS_LOOK_X = 900.0
+	NDefines.NFrontend.CREDITS_LOOK_Y = 0.0	
+	NDefines.NFrontend.CREDITS_LOOK_Z = 1450.0
