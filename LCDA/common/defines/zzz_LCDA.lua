4cfbc42feb7804991f342b0e6c0877bb4d1d7122
Arko <gg.guidon@gmail.com>
2018-08-30 03:49:53 +0200

    doubling vanilla diplo distance values instead of just 1,5%

--- LCDA/common/defines/zzz_LCDA.lua
+++ LCDA/common/defines/zzz_LCDA.lua
@@ -10,13 +10,13 @@
 -- NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_RELIGION_GROUP = 0 				-- Bonus to max diplo distance for being of the same religion group
 -- NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_CULTURE = 100 						-- Bonus to max diplo distance for being of the same culture
 -- NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_CULTURE_GROUP = 100				-- Bonus to max diplo distance for being of the same culture group
-	NDefines.NDiplomacy.MAX_DIPLO_DISTANCE = 1050									-- Most diplomacy is disallowed if two rulers are too distant
-	NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_RELIGION = 150						-- Bonus to max diplo distance for being of the same religion
+	NDefines.NDiplomacy.MAX_DIPLO_DISTANCE = 1400									-- Most diplomacy is disallowed if two rulers are too distant
+	NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_RELIGION = 200						-- Bonus to max diplo distance for being of the same religion
 	NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_RELIGION_GROUP = 0 				-- Bonus to max diplo distance for being of the same religion group
-	NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_CULTURE = 150 						-- Bonus to max diplo distance for being of the same culture
-	NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_CULTURE_GROUP = 150				-- Bonus to max diplo distance for being of the same culture group
+	NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_CULTURE = 200 						-- Bonus to max diplo distance for being of the same culture
+	NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_CULTURE_GROUP = 200				-- Bonus to max diplo distance for being of the same culture group
 -- NDefines.NMilitary.MERCENARY_HIRE_DISTANCE_THRESHOLD = 600		-- Mercs will not be available if the province of origin is more distant from your capital
-	NDefines.NMilitary.MERCENARY_HIRE_DISTANCE_THRESHOLD = 900		-- Mercs will not be available if the province of origin is more distant from your capital
+	NDefines.NMilitary.MERCENARY_HIRE_DISTANCE_THRESHOLD = 1200		-- Mercs will not be available if the province of origin is more distant from your capital
 -- NDefines.NMilitary.SHATTERED_RETREAT_PREFERRED_PROVINCES = 4				-- Units will try to move at least this many provinces away in shattered retreat
 -- NDefines.NMilitary.SHATTERED_RETREAT_MAX_PROVINCES = 10				-- Units can not move longer than this many provinces during a shattered retreat
 	NDefines.NMilitary.SHATTERED_RETREAT_PREFERRED_PROVINCES = 2				-- Units will try to move at least this many provinces away in shattered retreat
@@ -24,4 +24,4 @@
 -- NDefines.NGraphics.CITY_SPRAWL_AMOUNT = 1 -- Size of cities, higher gives larger cities
 	NDefines.NGraphics.CITY_SPRAWL_AMOUNT = 1 -- Size of cities, higher gives larger cities
 -- NDefines.NAI.MIL_AI_COUNTER_INVADE_DISTANCE_THRESHOLD = 120			-- Defenders in wars will not counter invade past this distance
-	NDefines.NAI.MIL_AI_COUNTER_INVADE_DISTANCE_THRESHOLD = 180			-- Defenders in wars will not counter invade past this distance
+	NDefines.NAI.MIL_AI_COUNTER_INVADE_DISTANCE_THRESHOLD = 240			-- Defenders in wars will not counter invade past this distance
